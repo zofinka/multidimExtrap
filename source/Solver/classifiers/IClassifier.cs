40f9cc9670df283f462213dfa5fb061f11db1a9f
Smirnov <egor.smirnov@intel.com>
2019-10-13 22:47:56 +0300

    Added RandomForest from alglib for point analysis. Added very silly simple example of algorithm usage

---
+++ source/Solver/classifiers/IClassifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+namespace Classifiers
+{
+    public struct LabeledData
+    {
+        public double[] data; // add generic type instead of double.
+        public int label;
+
+        public LabeledData(double[] data, int label)
+        {
+            this.data = data;
+            this.label = label;
+        }
+    }
+    public abstract class AClassifierParams
+    {
+        // Training set
+        public LabeledData[] XY { get; set; }
+
+        public AClassifierParams(LabeledData[] xy)
+        {
+            XY = xy;
+        }
+    }
+
+    interface IClassifier
+    {
+        void train(AClassifierParams param);
+        void infer(double[] x, out int label);
+        void validate(LabeledData[] XY, out double modelPrecision);
+    }
+}
+}
